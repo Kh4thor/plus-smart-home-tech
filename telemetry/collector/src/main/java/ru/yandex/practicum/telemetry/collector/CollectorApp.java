7440bc69a5924afc2ff022e361823c9e092053fc
Egor Malyshev <ya.krumm@ya.ru>
2025-11-30 12:20:16 +0300

    #14 Добавлен maven-jar-plugin в pom/collector

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/CollectorApp.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/CollectorApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class Collector {
+public class CollectorApp {
     public static void main(String[] args) {
-        SpringApplication.run(Collector.class, args);
+        SpringApplication.run(CollectorApp.class, args);
     }
 }
