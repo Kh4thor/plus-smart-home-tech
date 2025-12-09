6af2a2ad814c1e975b6c82bf30b851d1e1b78ddd
Egor Malyshev <ya.krumm@ya.ru>
2025-12-09 16:45:23 +0300

    #4 Добавлен BaseAvroDeserializer

---
+++ telemetry/aggregator/src/java/ru/yandex/practicum/aggregator/BaseAvroDeserializer.java
@@ -0,0 +1,24 @@
+package java.ru.yandex.practicum.aggregator;
+
+import org.apache.avro.Schema;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
+
+    public BaseAvroDeserializer(Schema schema) {
+        this(DecoderFactory.get(), schema);
+    }
+
+    public BaseAvroDeserializer(DecoderFactory decoderFactory, Schema schema) {
+        // ...
+    }
+
+    // ...
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        // Код десериализации двоичных данных
+    }
+}
