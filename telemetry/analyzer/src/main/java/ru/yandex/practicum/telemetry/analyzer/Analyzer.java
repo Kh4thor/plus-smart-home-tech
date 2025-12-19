416d7e48287f5bd1a23c395ef386bdcf884e743c
Egor Malyshev <ya.krumm@ya.ru>
2025-12-19 12:13:23 +0300

    #12 Дополнены конфигурации в application.properties

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/Analyzer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/Analyzer.java
@@ -12,19 +12,19 @@ public class Analyzer {
         ConfigurableApplicationContext context =
                 SpringApplication.run(Analyzer.class, args);
 
-        final HubEventProcessor hubEventProcessor =
-                context.getBean(HubEventProcessor.class);
-        SnapshotProcessor snapshotProcessor =
-                context.getBean(SnapshotProcessor.class);
-
-        // запускаем в отдельном потоке обработчик событий
-        // от пользовательских хабов
-        Thread hubEventsThread = new Thread(hubEventProcessor);
-        hubEventsThread.setName("HubEventHandlerThread");
-        hubEventsThread.start();
-
-        // В текущем потоке начинаем обработку
-        // снимков состояния датчиков
-        snapshotProcessor.start();
+//        final HubEventProcessor hubEventProcessor =
+//                context.getBean(HubEventProcessor.class);
+//        SnapshotProcessor snapshotProcessor =
+//                context.getBean(SnapshotProcessor.class);
+//
+//        // запускаем в отдельном потоке обработчик событий
+//        // от пользовательских хабов
+//        Thread hubEventsThread = new Thread(hubEventProcessor);
+//        hubEventsThread.setName("HubEventHandlerThread");
+//        hubEventsThread.start();
+//
+//        // В текущем потоке начинаем обработку
+//        // снимков состояния датчиков
+//        snapshotProcessor.start();
     }
 }
