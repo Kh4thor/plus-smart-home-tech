ef00ae80e50701735418ca48dba44b19d0d51518
Egor Malyshev <ya.krumm@ya.ru>
2026-01-17 11:13:59 +0300

    #37 Добавлен feign-client для модуля shopping-cart

---
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/feign/FeignClientShoppingCart.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.feign;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.dto.ChangeQuantityDto;
+import ru.yandex.practicum.dto.ShoppingCartDto;
+
+import java.util.List;
+import java.util.UUID;
+
+@FeignClient(name = "shopping-cart", path = "/api/v1/shopping-cart")
+public interface FeignClientShoppingCart {
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ShoppingCartDto putShoppingCart(
+            @RequestBody @Valid ShoppingCartDto shoppingCartDto,
+            @RequestParam @NotBlank String username);
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean deleteShoppingCart(@RequestParam @NotBlank String username);
+
+    @PostMapping("/remove")
+    @ResponseStatus(HttpStatus.OK)
+    public ShoppingCartDto removeShoppingCart(
+            @RequestParam @NotBlank String username,
+            @RequestBody List<UUID> productIds);
+
+    @PostMapping("change-quantity")
+    public ShoppingCartDto changeQuantity(
+            @RequestParam @NotBlank String username,
+            @RequestBody ChangeQuantityDto changeQuantityDto);
+}
