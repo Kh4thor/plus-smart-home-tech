cc6c964f6ed07d85e7a3604113eb9b49dc341bbb
Egor Malyshev <ya.krumm@ya.ru>
2026-01-18 18:36:32 +0300

    #44 Исправлены ошибки регистрации модулей server-config shopping-cart shopping-store и warehouse в модуле discovery-server (eureka)

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApp.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApp.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ShoppingStoreApp {
