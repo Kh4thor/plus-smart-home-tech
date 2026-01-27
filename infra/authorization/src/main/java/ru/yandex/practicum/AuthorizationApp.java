3962cedbd1d4296ca617bd26cb96e9981c6dcced
Egor Malyshev <ya.krumm@ya.ru>
2026-01-27 12:10:40 +0300

    #1 Добавлен модуль order и его dto-классы

---
+++ infra/authorization/src/main/java/ru/yandex/practicum/AuthorizationApp.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApp.class, args);
+    }
+}
