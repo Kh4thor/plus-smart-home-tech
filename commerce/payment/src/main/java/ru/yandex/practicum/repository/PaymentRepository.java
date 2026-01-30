6f351cfd087086b8f6be2147341d45cba0477daf
Egor Malyshev <ya.krumm@ya.ru>
2026-01-30 09:53:47 +0300

    #17 Добавлен PaymentRepository

--- commerce/payment/src/main/java/ru/yandex/practicum/repository/PaymentRepository.java
+++ commerce/payment/src/main/java/ru/yandex/practicum/repository/PaymentRepository.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.controller.model.Payment;
+import ru.yandex.practicum.model.Payment;
 
 import java.util.UUID;
 
---
+++ commerce/payment/src/main/java/ru/yandex/practicum/service/PaymentService.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.repository.PaymentRepository;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+}
